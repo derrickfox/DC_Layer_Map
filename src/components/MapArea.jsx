4bae9e0e5e4f80e0cf72b607d1c16d97f419df5d
Derrick Fox <fox.derrick@gmail.com>
2026-04-23 16:40:04 -0400

    Recalibrate zoom sensitivity for middle ground

--- src/components/MapArea.jsx
+++ src/components/MapArea.jsx
@@ -631,10 +631,10 @@ const MapArea = ({ activeLayers, geoJsonData, hiddenNeighborhoods, dcBoundary, f
       minZoom={11}
       style={{ height: '100%', width: '100%', zIndex: 0 }}
       zoomControl={false}
-      wheelPxPerZoomLevel={120}
+      wheelPxPerZoomLevel={90}
       zoomSnap={0.1}
       zoomDelta={0.1}
-      wheelDebounceTime={40}
+      wheelDebounceTime={30}
     >
       <TileLayer
         url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
